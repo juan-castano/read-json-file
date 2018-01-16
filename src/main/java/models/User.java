98b766bd058a7c529f22288530d34542c9130f51
Juan Castaño <juancastano.ing@gmail.com>
2018-01-16 01:26:02 -0500

    Initial commit for reading json file application

---
+++ src/main/java/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+public class User {
+
+    public User() {}
+
+    public String name;
+
+    @Override
+    public String toString() {
+        return "Usuario: " + name;
+    }
+
+}
